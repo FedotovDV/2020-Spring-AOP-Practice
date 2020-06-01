436fbb48a73ef89595ebc660ca5142dc3b77d561
Dmitriy <fed3375546@yandex.ru>
2020-06-01 23:57:19 +0300

    Match on method parameters(using wildcards) with any number of arguments

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,10 +12,10 @@ public class MyDemoLoggingAspect {
 
 	// let's start with an @Before advice
 
-	@Before("execution(* add*(com.luv2code.aopdemo.Account))")
+	@Before("execution(* add*(com.luv2code.aopdemo.Account, ..))")
 	public void beforeAddAccountAdvice() {
 
-		System.out.println("\n=====>>> Executing @Before advice on add*(com.luv2code.aopdemo.Account)");
+		System.out.println("\n=====>>> Executing @Before advice on add*(com.luv2code.aopdemo.Account, ..)");
 
 	}
 }
