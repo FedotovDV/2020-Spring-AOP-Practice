57b626435546da8029c1dca9c84cd6039bcf5074
Dmitriy <fed3375546@yandex.ru>
2020-06-03 19:56:36 +0300

    @Pointcut: reuse a pointcut expression

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,21 +1,26 @@
 package com.luv2code.aopdemo.aspect;
 
+
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-	// this is where we add all of our related advices for logging
-
-	// let's start with an @Before advice
-
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
-	public void beforeAddAccountAdvice() {
-
-		System.out.println("\n=====>>> Executing @Before advice on com.luv2code.aopdemo.dao.*.*(..)");
-
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Before("forDaoPackage()")
+	public void beforeAddAccountAdvice() {		
+		System.out.println("\n=====>>> Executing @Before advice on method");		
 	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Performing API analytics");		
+	}
+	
 }
